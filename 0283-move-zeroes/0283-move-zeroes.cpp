5c72c80bbc91368b2b35723ca819b4c202aa1e43
Khan Siraj Abdullah <siraj34@eng.rizvi.edu.in>
2026-07-04 15:53:58 +0530

    Time: 480 ms (5.1%), Space: 23.9 MB (55.67%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -5,9 +5,7 @@ public:
         for (int i = 0; i <n-1; i++) {
             for (int j=0; j<n-i-1; j++){
                 if(nums[j]==0 && nums[j+1] != 0) {
-                    int temp = nums[j];
-                    nums[j] = nums[j+1];
-                    nums[j+1] = temp;
+                    swap(nums[j], nums[j+1]);
 
                 }
             }
