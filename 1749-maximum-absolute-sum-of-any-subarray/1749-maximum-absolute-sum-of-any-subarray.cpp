1feb511e5b3960512af9e0a70ab2d7a8c90349e7
Khan Siraj Abdullah <siraj34@eng.rizvi.edu.in>
2026-07-27 21:27:55 +0530

    Time: 0 ms (100%), Space: 45.3 MB (24.55%) - LeetHub

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
@@ -2,22 +2,17 @@ class Solution {
 public:
     int maxAbsoluteSum(vector<int>& nums) {
         int n = nums.size();
-        int currSubSum = nums[0];
         int maxSubSum = nums[0];
-
-        // Kadane's algoto find maxsubsum
-        for(int i = 1; i < n; i++){
-            currSubSum = max(nums[i], currSubSum + nums[i]);
-            maxSubSum = max(maxSubSum , currSubSum);
-        }
-
-        // Kadane's algoto find maxsubsum
         int minSubSum = nums[0];
-        currSubSum = nums[0]; // reset
+        int currSubSumMax = nums[0];
+        int currSubSumMin = nums[0];
 
-        for(int i = 1; i < n; i++){
-            currSubSum = min(nums[i], currSubSum + nums[i]);
-            minSubSum = min(minSubSum , currSubSum);
+        for (int i = 1; i < n; i++) {
+            currSubSumMax = max(nums[i], currSubSumMax+nums[i]);
+            maxSubSum = max(maxSubSum, currSubSumMax);
+
+            currSubSumMin = min(nums[i], currSubSumMin+nums[i]);
+            minSubSum = min (minSubSum, currSubSumMin);
         }
         return max(abs(maxSubSum), abs(minSubSum));
     }
