61f6afbaa8bb0c1d8e104326b90c7e904c886015
Khan Siraj Abdullah <siraj34@eng.rizvi.edu.in>
2026-08-02 21:05:11 +0530

    Time: 0 ms (100%), Space: 21.8 MB (71.38%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int arraySum = 0;
+
+        for(int i = 0; i < n; i++){
+            arraySum = arraySum + nums[i];
+        }
+        int zeroToNsum = n*(n+1)/2;
+    return zeroToNsum - arraySum;
+    }
+};
