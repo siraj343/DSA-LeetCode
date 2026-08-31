742c71915f78739074f389defdf8762a7829bab6
Khan Siraj Abdullah <siraj34@eng.rizvi.edu.in>
2026-08-31 22:48:36 +0530

    Time: 128 ms (16.71%), Space: 134.2 MB (19.8%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        unordered_set<int> st;
+
+        int i = 0, j = 0;
+
+        while (j < n) {
+
+            // step-1
+            if (abs(i - j) > k) { // abs(i-j) <= k
+                st.erase(nums[i]);
+                i++; // shrink
+            }
+
+            // past me dekha hai nums[j] ?
+            if (st.find(nums[j]) != st.end()) {
+                return true;
+            }
+
+            st.insert(nums[j]);
+            j++;
+        }
+
+        return false;
+    }
+};
