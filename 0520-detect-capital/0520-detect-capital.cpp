763417bde5f6d0b906304a9c76726ef52ba03331
Khan Siraj Abdullah <siraj34@eng.rizvi.edu.in>
2026-09-23 20:45:00 +0530

    Time: 0 ms (100%), Space: 8.2 MB (15.71%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+
+    bool allCapital(string word) {
+        for (char &ch : word) {
+            if (ch < 'A' || ch > 'Z')
+            return false;
+        }
+
+        return true;
+    }
+
+    bool allSmall(string word) {
+        for (char &ch : word) {
+            if (ch < 'a' || ch > 'z')
+            return false;
+        }
+
+        return true;
+    }
+
+    bool detectCapitalUse(string word) {
+
+        if(allCapital(word) || allSmall(word) || allSmall(word.substr(1))){
+            return true;
+        }
+
+        return false;
+
+    }
+};
